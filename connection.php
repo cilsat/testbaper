3317b951c24cd9b71e87580e396aea0ebf7d86bc
cilsat <cil.satriawan@gmail.com>
2018-01-04 16:53:45 +0700

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $name = "baper_test";
-
-    $con = mysqli_connect($host, $user, $pass) or die("Koneksi ke database gagal!");
-    mysqli_select_db($con, $name) or die("Tidak ada database yang dipilih!");
-?>
