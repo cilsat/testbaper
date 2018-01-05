7a2e4e0f54ec3035c6f5f316d717616a51b582ef
Cil Satriawan <cil.satriawan@gmail.com>
2018-01-05 12:29:16 +0700

    Playing around with error_log

--- syssession.php
+++ syssession.php
@@ -9,6 +9,7 @@ class SysSession implements SessionHandlerInterface
     {
         $_link = connect();
         if ($_link) {
+            error_log("Opening ".$_COOKIE['PHPSESSID'], 0);
             $this->link = $_link;
             return true;
         } else {
@@ -17,13 +18,16 @@ class SysSession implements SessionHandlerInterface
     }
     public function close()
     {
+        error_log("Closing", 0);
         mysqli_close($this->link);
         return true;
     }
     public function read($id)
     {
-        $result = mysqli_query($this->link, "SELECT data FROM session WHERE id = '".$id."' AND expires > '".date('Y-m-d H:i:s')."'");
+        $date = date('Y-m-d H:i:s');
+        $result = mysqli_query($this->link, "SELECT data FROM session WHERE id = '".$id."' AND expires > '".$date."'");
         if ($row = mysqli_fetch_assoc($result)) {
+            error_log("Reading ".$id." expires ".$date, 0);
             return $row['data'];
         } else {
             return "";
@@ -31,12 +35,11 @@ class SysSession implements SessionHandlerInterface
     }
     public function write($id, $data)
     {
-        echo "<div>".$id."</div>";
-        echo "<div>".$data."</div>";
         $DateTime = date('Y-m-d H:i:s');
         $NewDateTime = date('Y-m-d H:i:s', strtotime($DateTime.' + 1 hour'));
         $result = mysqli_query($this->link, "REPLACE INTO session SET id = '".$id."', expires = '".$NewDateTime."', data = '".$data."'");
         if ($result) {
+            error_log("Writing ".$id." data ".$data, 0);
             return true;
         } else {
             return false;
@@ -46,6 +49,7 @@ class SysSession implements SessionHandlerInterface
     {
         $result = mysqli_query($this->link, "DELETE FROM session WHERE id ='".$id."'");
         if ($result) {
+            error_log("Destroying ".$id, 0);
             return true;
         } else {
             return false;
@@ -55,6 +59,7 @@ class SysSession implements SessionHandlerInterface
     {
         $result = mysqli_query($this->link, "DELETE FROM session WHERE ((UNIX_TIMESTAMP(expires) + ".$maxlifetime.") < ".$maxlifetime.")");
         if ($result) {
+            error_log("GC ".$maxlifetime, 0);
             return true;
         } else {
             return false;
@@ -63,6 +68,5 @@ class SysSession implements SessionHandlerInterface
 }
 $handler = new SysSession();
 session_set_save_handler($handler, true);
-session_start();
-echo "<div>".$_COOKIE['PHPSESSID']."</div>";
+session_start(['use_only_cookies' => 0, 'use_trans_id' => 1]);
 ?>
